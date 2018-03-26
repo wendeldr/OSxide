1380f04600b17c6dd9069d8cf4bbf3dcda2b9cbf
Douglas <Flickdm@mail.uc.edu>
2018-03-26 14:54:01 -0400

    initial commit for timer

---
+++ src/boards/peripherals/timer.rs
@@ -0,0 +1,34 @@
+extern crate cortex_m;
+
+use super::PERIPH;
+
+pub struct Timer {
+
+}
+
+impl Timer {
+    pub fn set_timer(us: u32) {
+        cortex_m::interrupt::free(|cs| {
+            if let Some(p) = PERIPH.borrow(_cs).borrow().as_ref() {
+                // Set timer to 32 bit and prescalar to 4
+                p.TIMER0.bitmode.write(|w| w.bitmode()._32bit());
+                p.TIMER0.prescaler.write(|w| unsafe { w.prescaler().bits(4) });
+
+                //Set the Timer intent
+                p.TIMER0.intenset.write(|w| w.compare0().set());
+                p.TIMER0.shorts.write(|w| w.compare0_clear().enabled());
+                p.TIMER0.events_compare[0].write(|w| unsafe { w.bits(0) });
+
+                /* Program counter compare register with value */
+                p.TIMER0.cc[0].write(|w| unsafe { w.bits(us) });
+
+                /* Clear current counter value */
+                p.TIMER0.tasks_clear.write(|w| unsafe { w.bits(1) });
+
+                /* Start counting */
+                p.TIMER0.tasks_start.write(|w| unsafe { w.bits(1) });
+            }
+        });
+
+    }
+}
