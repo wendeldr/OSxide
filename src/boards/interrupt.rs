55083734153920d801f3fa1698ccee2cc7193895
Douglas <Flickdm@mail.uc.edu>
2018-03-26 23:11:58 -0400

    Timers working, found out how to work with raw pointers

--- src/boards/interrupt.rs
+++ src/boards/interrupt.rs
@@ -2,6 +2,7 @@
 use cortex_m::asm::bkpt;
 use cortex_m;
 
+use nrf51;
 use boards::nrf51dk::PERIPH;
 
 use Semaphore;
@@ -20,7 +21,7 @@ impl Interrupt {
                 // TODO we should be referencing the buttons array
                 
                 for i in 0..4 {
-                     let button = p.GPIOTE.events_in[i].read().bits() != 0;
+                     let _button = p.GPIOTE.events_in[i].read().bits() != 0;
                      
                      /* DO something*/
 
@@ -35,16 +36,16 @@ impl Interrupt {
 
     #[allow(non_snake_case)]
     pub fn TIMER0_IRQHandler() {
-        cortex_m::interrupt::free(|cs| {
-            if let Some(p) = PERIPH.borrow(cs).borrow().as_ref() {
-
+        cortex_m::interrupt::free(|_cs| {
                 /*Do Something*/
 
+            unsafe {
                 // clear the register
-                p.TIMER0.tasks_clear.write(|w| unsafe { w.bits(1) });
+                (*nrf51::TIMER0::ptr()).tasks_clear.write(|w| w.bits(1));
                 // clear the event
-                p.TIMER0.events_compare[0].write(|w| unsafe { w.bits(0) });
+                (*nrf51::TIMER0::ptr()).events_compare[0].write(|w| w.bits(0));
             }
+            bkpt();
         });
     }
 }
