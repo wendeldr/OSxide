7432ac9aad2edf617e71899d555398c87417b574
Douglas <Flickdm@mail.uc.edu>
2018-03-26 23:36:55 -0400

    moved frequency into new

--- src/boards/peripherals/timers.rs
+++ src/boards/peripherals/timers.rs
@@ -5,12 +5,13 @@ use nrf51::{self, timer0};
 #[derive(Clone, Copy)]
 pub struct Timer {
     timer: *const timer0::RegisterBlock,
-    i: usize
+    prescaler: u8,
+    i: usize,
 }
 
 impl Timer {
 
-    pub fn new(i: usize) -> Timer {
+    pub fn new(i: usize, frequency: u32) -> Timer {
 
         let timer = match i {
             0 => nrf51::TIMER0::ptr(),
@@ -18,14 +19,6 @@ impl Timer {
             2 => nrf51::TIMER2::ptr(),
             _ => panic!("Invalid Timer: {}", i),
         };
-
-        Timer {
-            timer,
-            i,
-        }
-    }
-
-    pub fn init(&self, us: u32, frequency: u32) {
 
         let prescaler = match frequency {
             16000000 => 0,
@@ -41,10 +34,19 @@ impl Timer {
             _ => panic!("Invalid frequency: {}", frequency),
         };
 
+        Timer {
+            timer,
+            prescaler,
+            i,
+        }
+    }
+
+    pub fn init(&self, us: u32) {
+
         // lets make sure we don't get interrupted..
         cortex_m::interrupt::free(|_cs| {
             unsafe {
-                (*self.timer).prescaler.write(|w| w.prescaler().bits(prescaler));
+                (*self.timer).prescaler.write(|w| w.prescaler().bits(self.prescaler));
                 (*self.timer).bitmode.write(|w| w.bitmode()._32bit());
                 //Set the Timer intent
                 (*self.timer).intenset.write(|w| w.compare0().set());
