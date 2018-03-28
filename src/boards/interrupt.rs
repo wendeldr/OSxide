f016fc37f7126e05425e1548cbeeda154c5572a7
Codi Burley <idocyelrub@gmail.com>
2018-03-27 21:15:07 -0400

    Added interrupt to the nrf51dk board, and now keep track of a pending interrupt. The idle task looks for that pending interrupt

--- src/boards/interrupt.rs
+++ src/boards/interrupt.rs
@@ -5,38 +5,74 @@ use cortex_m;
 use boards::nrf51dk::PERIPH;
 use boards::print::{print};
 
-pub struct Interrupt {
+#[derive(PartialEq, Clone)]
+pub enum PendingInterrupt {
+    Button1,
+    Button2,
+    Button3,
+    Button4
+}
 
+pub struct Interrupt {
+    pub pending_interrupt: Option<PendingInterrupt>
 }
 
 impl Interrupt {
 
+    pub fn new() -> Interrupt {
+        Interrupt { pending_interrupt: None }
+    }
+
     #[allow(non_snake_case)]
-    pub fn GPIOTE_IRQHandler() {
+    pub fn GPIOTE_IRQHandler(&mut self) {
         cortex_m::interrupt::free(|cs| {
             if let Some(p) = PERIPH.borrow(cs).borrow().as_ref() {
 
                 // TODO we should be referencing the buttons array
-                
                 for i in 0..4 {
                      let button = p.GPIOTE.events_in[i].read().bits() != 0;
 
                      if button {
-                        print("button");
+                        print("button pressed");
+                        self.set_button_pending_interrupt(i);
                      }
 
                      p.GPIOTE.events_in[i].write(|w| unsafe { w.bits(0) });
                 }
-                bkpt();
-
             }
         });
     }
 
     #[allow(non_snake_case)]
     pub fn TIMER0_IRQHandler() {
-        cortex_m::interrupt::free(|cs| {
+        cortex_m::interrupt::free(|_cs| {
             bkpt();
         });
+    }
+
+    fn set_button_pending_interrupt(&mut self, button_index: usize) {
+        let button_interrupts = [
+            PendingInterrupt::Button1,
+            PendingInterrupt::Button2,
+            PendingInterrupt::Button3,
+            PendingInterrupt::Button4
+        ];
+
+        self.pending_interrupt = Some(button_interrupts[button_index].clone());
+    }
+
+
+    pub fn consume_if_pending_interrupt(&mut self) -> Option<PendingInterrupt> {
+        let mut maybe_interrupt = None;
+
+        cortex_m::interrupt::free(|_cs| {
+            if let Some(ref interrupt) = self.pending_interrupt {
+                maybe_interrupt = Some(interrupt.clone());
+            }
+
+            self.pending_interrupt = None;
+        });
+
+        return maybe_interrupt;
     }
 }
